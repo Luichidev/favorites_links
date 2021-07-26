bcb0cf251ad02bd9fb8953dc013d2c5d50e2e524
@LuisArana <disconluis@gmail.com>
2021-07-26 19:26:07 +0200

    Se termino el controler y el index

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php 
-  include_one("controller/index.php");
+  include_once("controller/index.php");
 ?>
 
 <!DOCTYPE html>
@@ -10,11 +10,24 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="stylesheet" href="css/main.css">
-  <title>Mis Links Favoritos</title>
+  <title>Mis Links Favoritos | login</title>
 </head>
 
 <body>
-
+<main>
+  <?php
+    if($_SESSION["auth"]){
+      if($_SESSION["isAdmin"])
+        include_once("components/section_admin.php");
+      else 
+        include_once("components/section_users.php");
+    } elseif(!$baneado) {
+      include_once("components/form_login.php");
+    } else {
+      include_once("components/section_ban.php");
+    }   
+  ?>
+  </main>
 </body>
 
 </html>
